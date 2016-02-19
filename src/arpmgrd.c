a5aad368b96408c21d05d8b5f3d6a1e5f4585060
Mithun Halder HP <mithun.kum.halder@hpe.com>
2016-02-19 04:13:50 -0800

    chg: dev: restrict sending neighbor probe only for stale neighbors.
    TG-448
    
    Change-Id: Ic96d1eeb646c577ac75e26a9098ed0e1ac818912
    Signed-off-by: Mithun Halder HP <mithun.kum.halder@hpe.com>

--- src/arpmgrd.c
+++ src/arpmgrd.c
@@ -830,6 +830,7 @@ receive_neighbor_update(int sock)
             break;
 
         default:
+            VLOG_DBG("received nl msg_type %u", nlh->nlmsg_type);
             break;
         }
 
@@ -1105,10 +1106,8 @@ arpmgrd_reconfigure_neighbor(struct ovsdb_idl *idl)
         /* Check if dp_hit changed */
         if (cache_nbr->dp_hit != dp_hit) {
             /* If dp_hit is set, state in not reachable send probe */
-            if (dp_hit && strcmp(cache_nbr->state,
-                    OVSREC_NEIGHBOR_STATE_REACHABLE) &&
-                    strcmp(cache_nbr->state,
-                    OVSREC_NEIGHBOR_STATE_PERMANENT) &&
+            if (dp_hit &&(0 == strcmp(cache_nbr->state,
+                    OVSREC_NEIGHBOR_STATE_STALE)) &&
                     (nl_neighbor_sock > 0)) {
                 int family = AF_INET;
                 uint32_t dst[8];
